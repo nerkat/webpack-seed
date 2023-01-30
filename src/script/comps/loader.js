21a6809869ca56dae9f90f51858bb374c9bcc6ce
nerkat <nerkat@gmail.com>
2023-01-30 12:12:28 +0700

    init seed strip

--- src/script/comps/loader.js
+++ src/script/comps/loader.js
@@ -1,5 +1,3 @@
-// Listen for the onload event
-window.onload = function () {
-  // Hide loader
-  document.querySelector(".loader").style.display = "none";
-};
+// ==========================================================================
+// loader.js
+// ==========================================================================
